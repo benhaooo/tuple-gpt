53766e1a48a345ff648a3200d2907d529a1f402a
benhao <2274507134@qq.com>
2024-06-14 18:03:02 +0800

    fix：各种bug，重新会话等

--- src/stores/modules/chat.js
+++ src/stores/modules/chat.js
@@ -1,14 +1,14 @@
 import { defineStore } from "pinia";
 import { generateUniqueId, delay } from "@/utils/commonUtils";
-import { completions } from "@/apis"
-import { reactive } from "vue"
+import { completions } from "@/apis";
+import { reactive } from "vue";
 import useConfigStore from "@/stores/modules/config";
 import useUserStore from "@/stores/modules/user";
 import { storeToRefs } from "pinia";
 
 const configStore = useConfigStore();
 const userStore = useUserStore();
-const { moduleConfig } = storeToRefs(configStore)
+const { moduleConfig } = storeToRefs(configStore);
 
 const useSessionsStore = defineStore('sessions', {
     state: () => ({
@@ -17,21 +17,15 @@ const useSessionsStore = defineStore('sessions', {
         askprompt: {},
     }),
     getters: {
-        currentSession: (state) => {
-            return state.sessions.find(
-                (session) => session.id == state.currentSessionId
-            );
-        },
+        currentSession: (state) => state.sessions.find(session => session.id === state.currentSessionId),
     },
     actions: {
-
         // 新增会话
         addSession() {
             const session = {
                 id: generateUniqueId(),
                 messages: [],
                 ...moduleConfig.value
-
             };
             this.sessions.unshift(session);
             this.currentSessionId = session.id;
@@ -40,172 +34,115 @@ const useSessionsStore = defineStore('sessions', {
         // 删除会话
         deleteSession(index) {
             const curIndex = this.sessions.findIndex(session => session.id === this.currentSessionId);
-            if (index == curIndex) {
+            if (index === curIndex) {
                 if (this.sessions.length === 1) {
                     this.sessions[index].messages = [];
                     return;
                 }
-                if (index == this.sessions.length - 1) {
-                    this.currentSessionId = this.sessions[index - 1].id;
-                } else {
-                    this.currentSessionId = this.sessions[index + 1].id;
-                }
+                this.currentSessionId = index === this.sessions.length - 1 
+                    ? this.sessions[index - 1].id 
+                    : this.sessions[index + 1].id;
             }
-
             this.sessions.splice(index, 1);
         },
+
         setCurrentSession(id) {
             this.currentSessionId = id;
         },
+
         // 修改会话配置
-        updataSession(sessionConfig) {
+        updateSession(sessionConfig) {
             const curIndex = this.sessions.findIndex(session => session.id === this.currentSessionId);
             this.sessions[curIndex] = {
                 ...this.sessions[curIndex],
                 ...sessionConfig
-            }
+            };
         },
 
         // 删除消息
         deleteMessage(index) {
-            this.currentSession.messages.splice(index, 1)
+            this.currentSession.messages.splice(index, 1);
         },
+
         // 重新交流
         async reChat(index) {
-            const currentMsg = this.currentSession.messages[index]
-            currentMsg.content = ''
-            currentMsg.chatting = true
-
-            const slicedMessages = this.currentSession.messages.slice(0, index)
-            const systemMessage = this.getSytemMesg()
-            if (systemMessage) slicedMessages.unshift(systemMessage)
-            const data = {
-                model: this.currentSession.model,
-                messages: slicedMessages.map((msg) => {
-                    return {
-                        role: msg.role,
-                        content: msg.content,
-                    };
-                }),
-                stream: true,
-                max_tokens: this.currentSession.maxTokens,
-                temperature: this.currentSession.temperature,
-                top_p: this.currentSession.top_p,
-                presence_penalty: this.currentSession.presence_penalty,
-                frequency_penalty: this.currentSession.frequency_penalty,
-
-            }
-            const response = await completions(data, this.currentSession.model)
-            if (response.status === 200) {
-                return this.handleStreamMsg(response, currentMsg)
-            } else {
-                const res = await response.json()
-                const errContent = `发生错误：\n\`\`\`json\n${JSON.stringify(res, null, 2)}`
-                chattingMsg.content = errContent
-                delete chattingMsg.chatting
-            }
+            const currentMsg = this.currentSession.messages[index];
+            currentMsg.content = '';
+            currentMsg.chatting = true;
+            await this.sendMessageInternal(index, currentMsg.img ? { text: currentMsg.content, imgUrl: currentMsg.img } : { text: currentMsg.content });
         },
 
-        //发送图片
-        async senImgMessage(text, imgUrl) {
-            const session = this.sessions.find(
-                (session) => session.id == this.currentSessionId
-            );
-            session.model = "gpt-4o"
-            this.currentSession.messages.push({
-                role: "user",
-                content: text,
-                img: imgUrl,
-            });
-            const systemMessage = this.getSytemMesg()
-            const combinedMessages = this.currentSession.messages.filter((msg) => msg.role !== "assistant").map((msg) => {
-                return {
-                    role: msg.role,
-                    content: [
-                        {
-                            type: "text",
-                            text: msg.content,
-                        },
-                        {
-                            type: "image_url",
-                            image_url: {
-                                url: msg.img,
-                            },
-                        }
-
-                    ],
-                };
-            })
-            if (systemMessage) combinedMessages.unshift(systemMessage)
-            const data = {
-                model: this.currentSession.model,
-                messages: combinedMessages,
-                stream: true,
-                max_tokens: this.currentSession.maxTokens,
-                temperature: this.currentSession.temperature,
-                top_p: this.currentSession.top_p,
-                presence_penalty: this.currentSession.presence_penalty,
-                frequency_penalty: this.currentSession.frequency_penalty,
-            }
-            const chattingMsg = reactive({
-                role: "assistant",
-                content: "",
-                chatting: true
-            })
-            this.currentSession.messages.push(chattingMsg);
-
-
-            const response = await completions(data, this.currentSession.model)
-            if (response.status === 200) {
-                return this.handleStreamMsg(response, chattingMsg)
-            } else {
-                const res = await response.json()
-                const errContent = `发生错误：\n\`\`\`json\n${JSON.stringify(res, null, 2)}`
-                chattingMsg.content = errContent
-                delete chattingMsg.chatting
-            }
+        // 发送图片消息
+        async sendImgMessage(text, imgUrl, index = null) {
+            await this.sendMessageInternal(index, { text, imgUrl });
         },
+
         // 发送消息
-        async sendMessage(text) {
-            // userStore.account.surplusQuota--
-            this.currentSession.messages.push({
-                role: "user",
-                content: text,
-            });
-            const systemMessage = this.getSytemMesg()
-            const combinedMessages = [...this.currentSession.messages]
-            if (systemMessage) combinedMessages.unshift(systemMessage)
-            const data = {
-                model: this.currentSession.model,
-                messages: combinedMessages.map((msg) => {
-                    return {
-                        role: msg.role,
-                        content: msg.content,
-                    };
-                }),
-                stream: true,
-                max_tokens: this.currentSession.maxTokens,
-                temperature: this.currentSession.temperature,
-                top_p: this.currentSession.top_p,
-                presence_penalty: this.currentSession.presence_penalty,
-                frequency_penalty: this.currentSession.frequency_penalty,
-            }
-            const chattingMsg = reactive({
-                role: "assistant",
-                content: "",
-                chatting: true
-            })
-            this.currentSession.messages.push(chattingMsg);
-            const response = await completions(data, this.currentSession.model)
-            if (response.status === 200) {
-                return this.handleStreamMsg(response, chattingMsg)
-            } else {
-                const res = await response.json()
-                const errContent = `发生错误：\n\`\`\`json\n${JSON.stringify(res, null, 2)}`
-                chattingMsg.content = errContent
-                delete chattingMsg.chatting
+        async sendMessage(text, index = null) {
+            await this.sendMessageInternal(index, { text });
+        },
+
+        async sendMessageInternal(index, { text, imgUrl }) {
+            try {
+                const session = this.currentSession;
+                session.model = "gpt-4o";
+                if (index === null) {
+                    this.currentSession.messages.push({
+                        role: "user",
+                        content: text,
+                        img: imgUrl,
+                    });
+                } else {
+                    this.currentSession.messages[index].content = text;
+                    if (imgUrl) this.currentSession.messages[index].img = imgUrl;
+                }
+
+                const systemMessage = this.getSystemMsg();
+                const combinedMessages = this.currentSession.messages.filter(msg => msg.role !== "assistant").map(msg => ({
+                    role: msg.role,
+                    content: msg.img ? [
+                        { type: "text", text: msg.content },
+                        { type: "image_url", image_url: { url: msg.img } }
+                    ] : msg.content,
+                }));
+                if (systemMessage) combinedMessages.unshift(systemMessage);
+
+                const data = {
+                    model: session.model,
+                    messages: combinedMessages,
+                    stream: true,
+                    max_tokens: session.maxTokens,
+                    temperature: session.temperature,
+                    top_p: session.top_p,
+                    presence_penalty: session.presence_penalty,
+                    frequency_penalty: session.frequency_penalty,
+                };
+
+                const chattingMsg = index === null ? reactive({
+                    role: "assistant",
+                    content: "",
+                    chatting: true
+                }) : this.currentSession.messages[index];
+
+                if (index === null) this.currentSession.messages.push(chattingMsg);
+
+                const response = await completions(data, session.model);
+                if (response.status === 200) {
+                    await this.handleStreamMsg(response, chattingMsg);
+                } else {
+                    const res = await response.json();
+                    chattingMsg.content = `发生错误：\n\`\`\`json\n${JSON.stringify(res, null, 2)}\n\`\`\``;
+                    delete chattingMsg.chatting;
+                }
+            } catch (error) {
+                console.error("Error in sendMessageInternal:", error);
+                if (index !== null) {
+                    this.currentSession.messages[index].content = `发生错误：\n\`\`\`json\n${error.message}\n\`\`\``;
+                    delete this.currentSession.messages[index].chatting;
+                }
             }
         },
+
         // 处理流消息
         handleStreamMsg(response, chatting) {
             return new Promise(async (resolve, reject) => {
@@ -218,16 +155,16 @@ const useSessionsStore = defineStore('sessions', {
                             const { done, value } = await reader.read();
                             if (done) {
                                 controller.close();
-                                await delay(800)
-                                delete chatting["chatting"]
-                                resolve()
+                                await delay(800);
+                                delete chatting.chatting;
+                                resolve();
                                 return;
                             }
                             controller.enqueue(value);
                             const chunk = decoder.decode(value);
-                            buffer += chunk; //缓冲区
+                            buffer += chunk;
                             const lines = buffer.split('\n');
-                            buffer = lines.pop(); // 最后一个可能是不完整的
+                            buffer = lines.pop();
                             for (const line of lines) {
                                 if (line.trim().startsWith('data:')) {
                                     const jsonStr = line.trim().substring(5).trim();
@@ -235,10 +172,9 @@ const useSessionsStore = defineStore('sessions', {
                                         const json = JSON.parse(jsonStr);
                                         if (json.choices && json.choices[0] && json.choices[0].delta) {
                                             const content = json.choices[0].delta.content || '';
-                                            chatting.content += content
+                                            chatting.content += content;
                                             await delay(4);
                                         }
-
                                     }
                                 }
                             }
@@ -247,56 +183,60 @@ const useSessionsStore = defineStore('sessions', {
                         push();
                     },
                 });
-            })
-
+            });
         },
 
         // 预设消息
-        getSytemMesg() {
+        getSystemMsg() {
             return this.currentSession.system ? {
                 role: "system",
                 content: this.currentSession.system
-            } : null
+            } : null;
         },
 
-
-        // 清楚上下文
+        // 清除上下文
         clearCtx() {
             if (!this.currentSession.clearedCtx) {
                 this.currentSession.clearedCtx = [];
             } else {
-                this.currentSession.messages.unshift(...this.currentSession.clearedCtx)
-                delete this.currentSession.clearedCtx
-                return
+                this.currentSession.messages.unshift(...this.currentSession.clearedCtx);
+                delete this.currentSession.clearedCtx;
+                return;
             }
             this.currentSession.clearedCtx.push(...this.currentSession.messages);
             this.currentSession.messages = [];
         },
 
+        // 导入聊天记录
         importChat(e) {
-            const file = e.target.files[0]
-            const reader = new FileReader()
-            reader.readAsText(file)
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.readAsText(file);
             reader.onload = (e) => {
-                const data = JSON.parse(e.target.result)
-                this.sessions = data
-                this.currentSession = this.sessions[0]
-            }
+                const data = JSON.parse(e.target.result);
+                this.sessions = data;
+                this.currentSession = this.sessions[0];
+            };
         },
+
+        // 导出聊天记录
         exportChat() {
-            const data = JSON.stringify(this.sessions)
-            const blob = new Blob([data], { type: 'application/json' })
-            const url = URL.createObjectURL(blob)
-            const link = document.createElement('a')
-            link.href = url
-            link.download = 'chat.json'
-            link.click()
-            URL.revokeObjectURL(url)
+            const data = JSON.stringify(this.sessions);
+            const blob = new Blob([data], { type: 'application/json' });
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = 'chat.json';
+            link.click();
+            URL.revokeObjectURL(url);
         },
+
+        // 清除聊天记录
         clearChat() {
-            this.sessions = []
+            this.sessions = [];
         }
     },
     persist: true,
 });
-export default useSessionsStore
+
+export default useSessionsStore;
