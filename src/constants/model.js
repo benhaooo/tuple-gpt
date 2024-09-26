15bb9917d9cf3d9bc5f3cc85ff3f2b821cd714ec
benhao <2274507134@qq.com>
2024-09-26 09:34:33 +0800

    feat:o1 model

--- src/constants/model.js
+++ src/constants/model.js
@@ -12,12 +12,14 @@ const models = {
         'gpt-3.5-turbo',
         'gpt-4',
         'gpt-4o',
-        'gpt-4o-mini'
+        'gpt-4o-mini',
+        'o1-preview',
+        'o1-mini',
     ],
     claude: [
-        'claude-3-sonnet-20240229',
-        'claude-3-opus-20240229',
-        'claude-3-5-sonnet-20240620'
+        'claude-3-sonnet',
+        'claude-3-opus',
+        'claude-3-5-sonnet'
     ],
     gemini: [
         'gemini-pro',
