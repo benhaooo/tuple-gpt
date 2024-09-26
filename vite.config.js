d2c10d59a4d64b52378c094b56f7ebe2463dcd6e
benhao <2274507134@qq.com>
2024-09-26 10:27:22 +0800

    fix：ttf打包后缀补充

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-import { fileURLToPath, URL } from 'node:url'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -42,14 +41,15 @@ export default defineConfig({
           if (imgExts.some(ext => assetInfo.name.endsWith(ext))) {
             return 'img/[name]-[hash].[ext]';
           }
+          // 其他资产类型，设置默认路径
+          return 'assets/[name]-[hash][extname]';
         },
       },
-    }
+    },
   },
-
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
